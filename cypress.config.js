4f1f09c3d74aa999d234ca0820284cf3c1dff9f3
jvnaidu07 <jvnaidu07@gmail.com>
2025-08-24 20:05:32 +0530

    save latest changes

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    env: {
+      baseUrl: "https://www.globalsqa.com/demo-site/datepicker/",
+      dragAndDrop: "https://www.globalsqa.com/demo-site/draganddrop/"
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
