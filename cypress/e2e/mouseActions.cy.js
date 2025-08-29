ed0cc660df1e881cf967634631287a1d9388545e
jvnaidu07 <jvnaidu07@gmail.com>
2025-08-29 23:25:35 +0530

    added mouse over

---
+++ cypress/e2e/mouseActions.cy.js
@@ -0,0 +1,15 @@
+/// <reference types = "cypress"/>
+
+describe("", () => {
+    beforeEach(() => {
+        cy.visit(Cypress.env("mouseActions"))
+    })
+    it("mouse actions in cypress", () => {
+        cy.get("input[title='Enter First Name']").trigger('mouseover')
+        cy.get("input[title='Enter First Name']").should('have.attr', 'title');
+        cy.get("button[id='dblclick']").dblclick()
+        cy.contains("Double Click Action is Performed").should('have.text', 'Double Click Action is Performed')
+        cy.get("button[id='rightclick']").rightclick();
+        cy.contains("Registration Form Alert Popup Mouse Event").should('contain.text', 'Registration Form')
+    });
+});
